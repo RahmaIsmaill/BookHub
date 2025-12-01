c3de4117b0efd4a3aa52e93bf28eac03bd1f5c13
RahmaIsmaill <rahmaismail2010@gmail.com>
2025-12-01 22:37:32 +0200

    yalla bsm ellah

---
+++ src/main/java/com/example/kitabhub/KitabHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitabhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitabHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitabHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kitabhub/KitabHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitabhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitabHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
